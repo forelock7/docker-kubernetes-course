898813947db09c60258ffcf35110fbdfd2e8c41e
Volodymyr Chubenko <volodymyr.chubenko@sisense.com>
2023-06-29 20:44:47 +0300

    Kubernetes - Service url by kub env var(14 module)

--- 14-kub-network-01-starting-setup/users-api/users-app.js
+++ 14-kub-network-01-starting-setup/users-api/users-app.js
@@ -24,7 +24,8 @@ app.post("/signup", async (req, res) => {
 
   try {
     const hashedPW = await axios.get(
-      `http://${process.env.AUTH_ADDRESS}/hashed-password/` + password
+      `http://${process.env.AUTH_SERVICE_SERVICE_HOST}/hashed-password/` +
+        password
     );
     // const hashedPW = "dummy text";
     // since it's a dummy service, we don't really care for the hashed-pw either
@@ -57,7 +58,7 @@ app.post("/login", async (req, res) => {
   // normally, we'd find a user by email and grab his/ her ID and hashed password
   const hashedPassword = password + "_hash";
   const response = await axios.get(
-    `http://${process.env.AUTH_ADDRESS}/token/` +
+    `http://${process.env.AUTH_SERVICE_SERVICE_HOST}/token/` +
       hashedPassword +
       "/" +
       password
